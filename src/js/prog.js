b757240cb793bb57f36b14f40d5d5cac168d9695
odinokun <odinokun@gmail.com>
2017-09-08 08:48:02 +0300

    сделал popups

--- src/js/prog.js
+++ src/js/prog.js
@@ -1,5 +1,4 @@
-//====== Begin Programmer code ======
-
+// анимация денег
 $(window).scroll(function(){
   var target = $('#sec-02');
   var targetPos = target.offset().top;
@@ -11,6 +10,42 @@ $(window).scroll(function(){
   }
 });
 
+
+// анимация ракеты при клике
 $('.sec-08__btn').on('click', function() {
   $('.sec-08__rocket').addClass('active');
 });
+
+
+
+// begin popup open
+$('.popup-open').on('click', function() {
+  $('.popup, .popup__layer').fadeIn();
+});
+// end   popup open
+
+
+// begin popup send
+$('.popup__btn').on('click', function() {
+  $('.popup').fadeOut();
+  $('.popup-success').fadeIn();
+  return false;
+});
+// end   popup send
+
+// begin popup-success open
+$('.popup-send').on('click', function() {
+  $('.popup-success, .popup__layer').fadeIn();
+  return false;
+});
+// end   popup-success open
+
+
+// begin popup close
+$('.popup__layer, .popup-success__close, .popup__close').on('click', function() {
+  $('.popup, .popup-success, .popup__layer').fadeOut();
+});
+// end   popup close
+
+
+//====== Begin Programmer code ======
